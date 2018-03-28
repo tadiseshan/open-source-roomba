3af3fbc144efccdc3ba8c3b0bcf834d3a977c328
taraadiseshan <tara.adiseshan@gmail.com>
2018-03-28 12:50:45 -0700

    some code updates

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-var GitHubApi = require('github');
+var GitHubApi = require('@octokit/rest');
 var github = new GitHubApi({
     version: '3.0.0'
 });
 var secrets = require('./secrets.js');
 
-exports.handler = function(event, context) {
-    var githubEvent = event.Records[0].Sns.Message;
-    console.log('Received GitHub event:', githubEvent);
+module.exports.handler = function(event, context, callback) {
+  var githubEvent = event.Records[0].Sns.Message;
+  console.log('Received GitHub event:', githubEvent);
 
-    if (!githubEvent.hasOwnProperty('issue') || githubEvent.action !== 'opened') {
-        // Not an event for opening an issue
-        context.succeed();
-    }
+  if (!githubEvent.hasOwnProperty('issue') || githubEvent.action !== 'opened') {
+      // Not an event for opening an issue
+      return callback(null, 'Not an event for opening issue');
+  }
 
-    // Authenticate to comment on the issue
-    github.authenticate({
-        type: 'oauth',
-        token: secrets.token
-    });
-
-    var poster = githubEvent.issue.user.login;
-
-    github.issues.createComment({
-        user: githubEvent.repository.owner.login,
-        repo: githubEvent.repository.name,
-        number: githubEvent.issue.number,
-        body: "Hi @" + poster + "!\n" +
+  // Authenticate to comment on the issue
+  github.authenticate({ type: 'oauth', token: secrets.token})
+    .then(() => {
+      return github.issues.createComment({
+        owner: 'taraadiseshan',
+        repo: 'open-source-roomba',
+        number: 5,
+        body: "Hi @taraadiseshan!\n" +
               "\n" +
               "Thank you for your interest in this project! Unfortunately, we're " +
               "really busy at the moment, but we'll get to your issue as soon as " +
               "possible. Have a great day!"
-    }, context.done);
+      });
+    })
+    .then(() => callback())
+    .catch((err) => {return callback(err);});
 };
